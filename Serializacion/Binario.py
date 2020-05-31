5b34eb596c502a897be98ff8ce5b4c85c80755c5
Hernan Di Tano <hditano@outlook.com>
2020-05-31 11:40:51 -0300

    added

---
+++ Serializacion/Binario.py
@@ -0,0 +1,17 @@
+import pickle
+
+# lista_nombres=["pedro", "Ana", "Maria", "Isabel"]
+#
+# fichero_binario=open("lista_nombres", "wb")
+#
+# pickle.dump(lista_nombres, fichero_binario)
+#
+# fichero_binario.close()
+#
+# del(fichero_binario)
+
+fichero=open("lista_nombres", "rb")
+
+lista=pickle.load(fichero)
+
+print(lista)
